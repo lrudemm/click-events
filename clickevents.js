3d9e48598c184d0879420855d7757ee06d670d84
kburgosdavila <71047387+kburgosdavila@users.noreply.github.com>
2020-10-24 01:05:51 -0400

    Add files via upload

--- clickevents.js
+++ clickevents.js
@@ -1,5 +1,15 @@
-const button = document.querySelector("button");
+const button1 = document.querySelector("button");
+const body = document.querySelector("body")
+button1.addEventListener('click', responseToClick)
 
-button.addEventListener('click', function() {
-	alert("You clicked me");
-})
+function responseToClick() {
+   const pTag = document.createElement('p');
+   pTag.textContent = "Foo"
+   body.appendChild(pTag);
+
+
+}
+
+
+
+
