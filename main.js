d9e45309eda032b50fc5e9e872f8fabdd1e6240e
Andrew Vanegas <70233300+slvdrew@users.noreply.github.com>
2020-10-24 13:08:57 -0700

    Add files via upload
    added reference for the three buttons

---
+++ main.js
@@ -0,0 +1,6 @@
+// In your JavaScript, get 3 separate references to the buttons using the IDs you made and store them in variables. You should use either document.querySelector or document.getElementByID, both will work.
+const btn1 = document.querySelector("button");
+
+const btn2 = document.querySelector("button");
+
+const btn3 = document.querySelector("button");
